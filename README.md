039d7b2c1ddf116bf505bf9a1802c2f148a97927
Optinovation <hansean@gmail.com>
2013-07-26 09:03:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+PulsedIn is an web application that helps health care professionals better manage their careers.
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pulsedin::Application.config.session_store :cookie_store, key: '_pulsedin_session'
